d10063a946f1c2622f711a9f1136b43b03d524a8
muhammadyusuf364 <124252255+muhammadyusuf364@users.noreply.github.com>
2023-05-16 15:47:45 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Loader from './components/Loader'
+import Header from './components/Header'
+import MainContent from './components/main-content/MainContent'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+    <Loader />
+    <Header />
+    <MainContent />
+    <Footer />
+    </>
+    )
+  }
+
+  export default App
